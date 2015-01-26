34983a13956e7e6ef9c236ad45b259e1a9bee85a
Noitidart <noitidart@gmail.com>
2015-01-25 16:15:06 -0800

    Update bootstrap.js
    fixed it. i discovered that during uninstall proc the localization files are not available

--- bootstrap.js
+++ bootstrap.js
@@ -3,10 +3,12 @@ Cu.import('resource://gre/modules/Services.jsm');
 
 var stringBundle = Services.strings.createBundle('chrome://l10n/locale/global.properties?' + Math.random()); // Randomize URI to work around bug 719376
 
-function install(aData, aReason) {
+function startup(aData, aReason) {
+	// see here for aReason messages: https://gist.github.com/Noitidart/9025999#comment-1120821
 	if (aReason == ADDON_INSTALL || aReason == ADDON_UPGRADE || aReason == ADDON_DOWNGRADE) {
 		var currentUrl = Services.prefs.getCharPref('browser.newtab.url');
-		var result = Services.prompt.alert(null, stringBundle.GetStringFromName('confirm_pref_init'), stringBundle.formatStringFromName('install_msg', [currentUrl], 1));
+		
+		Services.prompt.alert(null, stringBundle.GetStringFromName('confirm_pref_init'), stringBundle.formatStringFromName('install_msg', [currentUrl], 1));
 		//store what it was on install
 		Services.prefs.setCharPref('extension.my-addon-id.newtab-url-pre-install', currentUrl);
 		
@@ -14,9 +16,13 @@ function install(aData, aReason) {
 		Services.prefs.setCharPref('browser.newtab.url', 'data:text/html,Mozilla Firefox!!!!');
 	}
 }
-function uninstall(aData, aReason) {
-	if (aReason == ADDON_UNINSTALL) {
+function shutdown(aData, aReason) {
+	// see here for aReason messages: https://gist.github.com/Noitidart/9025999#comment-1120821
+	Services.prompt.alert(null, 'debug msg', 'entered shutdown procedure, the aReaosn is:' + aReason + ' test equality to ADDON_UNINSTALL, answering question is it real uninstall? ::: ' + aReason);
+	if (aReason == ADDON_DISABLE) { // we have to do this in shutdown, instead of uninstall because during unisntall the localization file is not available
 		// real uninstall, this uninstall gets trigged on addon_upgrade/addon_downgrade as well thats why we have to do this test
+		//have to reimport stringBunlde during uninstall:
+		//stringBundle = Services.strings.createBundle('chrome://l10n/locale/global.properties?' + Math.random()); // Randomize URI to work around bug 719376
 		var currentUrl = Services.prefs.getCharPref('browser.newtab.url');
 		var preInstallUrl = Services.prefs.getCharPref('extension.my-addon-id.newtab-url-pre-install');
 		var result = Services.prompt.confirm(null, stringBundle.GetStringFromName('confirm_pref_reset'), stringBundle.formatStringFromName('uninstall_msg', [currentUrl, preInstallUrl], 2));
@@ -28,8 +34,8 @@ function uninstall(aData, aReason) {
 	}
 }
 
-function startup() {
-	Services.prompt.alert(null, stringBundle.GetStringFromName('startup_prompt_title'), stringBundle.GetStringFromName('startup_prompt_message'));
-}
+function install() {}
  
-function shutdown() {}
+function uninstall() {
+	// to trigger uninstall of bootstrap addon: go to addon manager, click "remove" on your addon. then close addon manager, this triggers the uninstall
+}
